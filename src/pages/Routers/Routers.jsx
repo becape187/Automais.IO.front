18d36cbd1073ba0b582664524a952d0f56b5a2d3
becape187 <bernardo@becape.com.br>
2026-01-09 23:19:08 -0300

    routers

--- src/pages/Routers/Routers.jsx
+++ src/pages/Routers/Routers.jsx
@@ -195,7 +195,7 @@ export default function Routers() {
           )}
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredRouters.map((router) => (
             <div 
               key={router.id} 
